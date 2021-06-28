04a773c9895ab336b88e71abf38c1d57d4a3ef20
michael.boumansour <michael.boumansour@agilecreatives.net>
2021-06-28 17:29:54 -0400

    Created a NoteKey class that contains takes the number of the note to play and the color that should be used for the background of the key

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(XylophoneApp());
+void main() {
+  runApp(XylophoneApp());
+}
 
 class XylophoneApp extends StatelessWidget {
   @override
@@ -8,9 +11,41 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              NoteKey(noteNum: 1, buttonColor: Colors.red),
+              NoteKey(noteNum: 2, buttonColor: Colors.orange),
+              NoteKey(noteNum: 3, buttonColor: Colors.yellow),
+              NoteKey(noteNum: 4, buttonColor: Colors.green),
+              NoteKey(noteNum: 5, buttonColor: Colors.teal),
+              NoteKey(noteNum: 6, buttonColor: Colors.blue),
+              NoteKey(noteNum: 7, buttonColor: Colors.purple),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class NoteKey extends StatelessWidget {
+  final int noteNum;
+  final Color buttonColor;
+  NoteKey({Key? key, required this.noteNum, required this.buttonColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      style: ButtonStyle(
+          fixedSize: MaterialStateProperty.all<Size>(Size(150, 30)),
+          backgroundColor: MaterialStateProperty.all<Color>(buttonColor),
+          enableFeedback: false),
+      onPressed: () {
+        final player = AudioCache();
+        player.play('note$noteNum.wav');
+      },
+      child: Text(''),
     );
   }
 }
